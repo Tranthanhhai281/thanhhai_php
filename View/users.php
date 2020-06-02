b5415a205c913c513622cff210478c6481e3723d
Tranthanhhai281 <Tranthanhhai281@gmail.com>
2020-06-03 00:29:21 +0700

    up code

---
+++ View/users.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Danh sách người dùng</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<h1>Danh sách người dùng</h1>
+<?php
+$user = new \Model\User();
+$current = $_SESSION['user'];
+?>
+<h2>Đang đăng nhập dưới tên: <?php echo $current['name']; ?></h2>
+<?php foreach ($user->getUsers() as $s): // ket hop ma html - php  // goi thuoc tinh cua doi tuong dung ky hieu -> ?>
+    <ul>
+        <li><a href="?route=edit&id=<?php echo $s["id"]; ?>"><?php echo $s["name"] ?></a></li>
+        <li><?php echo $s["email"] ?></li>
+        <li><?php echo $s["password"] ?></li>
+        <li><a href="?route=delete&id=<?php echo $s["id"]; ?>">delete</a></li>
+    </ul>
+<?php endforeach;// lam the nay de khoi lan voi dau {} cua bon khac nhu if else ... ?>
+
+</body>
+</html>
---
+++ lib/Connector.php
@@ -0,0 +1,13 @@
+<?php
+namespace lib;
+
+class Connector{
+    private static $conn;
+
+    public static function connector(){
+        if(is_null(self::$conn)){
+            self::$conn = connect();
+        }
+        return self::$conn;
+    }
+}
